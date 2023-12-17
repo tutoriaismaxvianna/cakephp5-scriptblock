9f531ca443acd19eaf4eb398e44a0f5febcb2850
Tutoriais por Max Vianna <tutoriaismaxvianna@gmail.com>
2023-12-16 21:54:47 -0300

    Javascript "Hello World" no template default.php

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -21,6 +21,10 @@
     <?= $this->Html->script('https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js') ?>
     <?= $this->fetch('script') ?>
 
+    <script>
+        alert('Hello World!');
+    </script>
+
 </body>
 
 </html>
